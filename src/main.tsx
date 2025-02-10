03c9699d17df17abe84952aa0e4915c53f9a63ed
Ryry <ryan.tillman-french@ascentcloud.io>
2025-02-10 11:52:30 -0500

    fixing routing issue for links directly to pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,29 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, useLocation } from "react-router";
+import "./index.css";
+import App from "./App.tsx";
+import { useEffect } from "react";
 
 const root = document.getElementById("root");
 
+function RouteHandler() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const path = params.get("p");
+    if (path) {
+      const fixedPath = path.startsWith("/") ? path : "/" + path;
+      window.history.replaceState({}, "", fixedPath);
+    }
+  }, [location]);
+
+  return null;
+}
+
 ReactDOM.createRoot(root!).render(
   <BrowserRouter>
+    <RouteHandler />
     <App />
   </BrowserRouter>
 );
